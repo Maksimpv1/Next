a723ca44d63f3dc45b486a92213fbc24cfd563be
Maksim noyt <maksimpv1@bk.ru>
2023-12-01 22:26:43 +0300

    next

--- next-app/src/app/(accaunt)/admin/page.tsx
+++ next-app/src/app/(accaunt)/admin/page.tsx
@@ -1,3 +1,5 @@
+/*'use client' это чтобы сделать компонент клиенским*/
+
 export default function AdminPage () {
     return(
         <div>Admin</div>
--- next-app/src/app/(accaunt)/layout.tsx
+++ next-app/src/app/(accaunt)/layout.tsx
@@ -4,7 +4,9 @@ export default function DashboardLayout ({children} : PropsWithChildren<unknown>
     return(
         <div>
             <h1>DashBoard Layout</h1>
+            <header>header</header>
             {children}
+            <footer>footer</footer>
         </div>
     )
 }
